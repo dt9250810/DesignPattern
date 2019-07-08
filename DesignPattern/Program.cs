a9883ede2d36661d1903eaafb822be80321da514
Bruce <dt9250810@gmail.com>
2019-07-08 20:53:04 +0800

    init

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOpen = true;
+            Console.WriteLine("Console Design Pattern in C# by Bruce\r");
+
+            while (isOpen)
+            {
+                Console.Write("> ");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "a":
+                        Console.WriteLine("a");
+                        break;
+                    case "s":
+                        Console.WriteLine("a");
+                        break;
+                    case "m":
+                        Console.WriteLine("a");
+                        break;
+                    case "d":
+                        Console.WriteLine("a");
+                        break;
+                    case "0":
+                        isOpen = false;
+                        break;
+                }
+            }
+            Console.WriteLine("Bye~");
+            Console.ReadKey();
+        }
+    }
+}
